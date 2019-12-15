f45302e70f40cd562b149acceb347c8561be1061
B1Z0N <kolausf@gmail.com>
2019-12-15 02:58:25 +0200

    Added projects page

---
+++ script/projects/list.js
@@ -0,0 +1,68 @@
+const projects = [
+    {
+        'title' : 'This site',
+        'date' : '2019-12-15',
+        'link' : 'https://github.com/B1Z0N/homepage',
+        'language' : 'HTML | CSS | JS',
+    },
+    {
+        'title' : 'Job Search Site',
+        'date' : '2019-11-25',
+        'link' : 'https://github.com/B1Z0N/workspide',
+        'language' : 'Python | Django',
+    },
+    {
+        'title' : 'Arithmetic Notation Converter',
+        'date' : '2019-09-17',
+        'link' : 'https://github.com/B1Z0N/NotationConverter',
+        'language' : 'C++',
+    },
+    {
+        'title' : 'C++ developer resources',
+        'date' : '2019-08-21',
+        'link' : 'https://github.com/B1Z0N/cpp-dev-resources',
+        'language' : 'Markdown',
+    },
+    {
+        'title' : 'Macros for Turing machine',
+        'date' : '2019-07-28',
+        'link' : 'https://github.com/B1Z0N/turingmachine',
+        'language' : 'Python',
+    },
+    {
+        'title' : 'Sorting Benchmark',
+        'date' : '2019-06-30',
+        'link' : 'https://github.com/B1Z0N/SortingBenchmark',
+        'language' : 'C++',
+    },
+    {
+        'title' : 'GeometryLIB',
+        'date' : '2019-04-27',
+        'link' : 'https://github.com/B1Z0N/KPI-geometrylib',
+        'language' : 'C++',
+    },
+    {
+        'title' : 'Custom bootloader',
+        'date' : '2018-08-23',
+        'link' : 'https://github.com/B1Z0N/CustomBootloader',
+        'language' : 'NASM (Assembly)',
+    },
+    {
+        'title' : 'cmd utility for commenting code',
+        'date' : '2018-07-25',
+        'link' : 'https://github.com/B1Z0N/KOmment',
+        'language' : 'shell',
+    },
+    {
+        'title' : 'Analysis and cracking of PRNG',
+        'date' : '2018-05-24',
+        'link' : 'https://github.com/B1Z0N/likeKOORSUCH0',
+        'language' : 'C',
+    },
+    {
+        'title' : 'Numerical system converter (even negative)',
+        'date' : '2017-08-15',
+        'link' : 'https://github.com/B1Z0N/numerical_sys',
+        'language' : 'C',
+    },
+];
