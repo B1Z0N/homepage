9799abb04c81bc582e2524d05892234bdaf63748
B1Z0N <kolausf@gmail.com>
2019-12-15 15:14:22 +0200

    Added filters to contrib page

--- script/contributions/script.js
+++ script/contributions/script.js
@@ -17,12 +17,14 @@ const contribTemplate = `
             </a>
         `;
 
+var lang, contribStatus;
 // display contributions data from contributions object
 
-const displayContributions = () => {
+const displayContributions = arr => {
   const contribList = document.getElementById("project-list");
+  contribList.innerHTML = "";
   var i = Math.floor(Math.random() * 1000);
-  for (let contrib of contributions) {
+  for (let contrib of arr) {
     contribList.innerHTML += contribTemplate.format(
       contrib["link"],
       (++i % 6) + 1,
@@ -33,4 +35,50 @@ const displayContributions = () => {
   }
 };
 
-window.onload = displayContributions;
+const languageFilterBy = arr => {
+  var res = [];
+  for (let contrib of arr) {
+    var include;
+    if (typeof contrib["language"] === "string") {
+      include = contrib["language"] === lang || contrib["language"] === "any";
+    } else {
+      include = contrib["language"].includes(lang);
+    }
+
+    if (lang === "any" || include === true) {
+      res.push(contrib);
+    }
+  }
+
+  return res;
+};
+
+const statusFilterBy = arr => {
+  var res = [];
+  for (let contrib of contributions) {
+    var include;
+    if (
+      contribStatus === "any" ||
+      (typeof contrib["status"] === "string" && contrib["status"] === contribStatus)
+    ) {
+      res.push(contrib);
+    }
+  }
+
+  return res;
+};
+
+const fullFilter = (_lang = -1, _status = -1) => {
+  lang = _lang === -1 ? lang : _lang;
+  contribStatus = _status === -1 ? contribStatus : _status;
+  displayContributions(languageFilterBy(statusFilterBy(contributions)));
+};
+
+window.onload = () => {
+  lang = document.getElementById("languageSelect").options;
+  lang = lang[lang.selectedIndex].value;
+  contribStatus = document.getElementById("statusSelect").options;
+  contribStatus = contribStatus[contribStatus.selectedIndex].value;
+
+  fullFilter();
+};
